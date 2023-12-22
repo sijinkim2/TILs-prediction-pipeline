4f07752ed6810c123a184d7276650ff78be61689
sijinkim2 <153062841+sijinkim2@users.noreply.github.com>
2023-12-22 10:04:17 +0900

    Update gray.py

--- gray.py
+++ gray.py
@@ -1,5 +1,4 @@
 import os
-#os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = pow(2,40).__str__()
 from PIL import Image
 import numpy as np
 from pathlib import Path
@@ -35,71 +34,6 @@ for i in files:
                 dst), img)
 
     a = a + 1
-    #if val_img(img == 255) > 0.6:
-    #    img_array = np.array(img)
-    #plt.hist(img_array)
-    #plt.show()
-    #cv2.imwrite('/home/skim/NAS01/Databank/public/2_Pathology/whole_body/cropped_WSI/TCGA-BRCA/TCGA-A2-A0YK-01A-02-BSB/gray_scale_thre/%s' % (dst), img)
-    #img.save(("gray/%s" % (dst)))
 
 
 
-'''
-for i in files:
-    path_img = os.path.join('/home/skim/NAS01/Databank/2_Pathology/public/TIGER/wsitils/images', i)
-    file_name = Path(path_img).stem
-    path_mask = os.path.join(
-        '/home/skim/NAS01/Databank/2_Pathology/public/TIGER/wsitils/tissue-masks',
-        i)
-    #img = Image.open(path_img)
-    #img_mask = Image.open(path_mask)
-    img = cv2.imread
-    img_mask = cv2.imread(path_mask)
-    maskarray = np.array(img_mask)
-    #img = cv2.imread(path)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    a = 1
-    h, w, c = img_mask.shape
-    #height
-    height = round(1 + (h / 256))
-    difference_h = h / height
-    overlap_h = (256 - difference_h) / (height - 1)
-
-    #width
-    width = round(1 + (w / 256))
-    difference_w = w / width
-    overlap_w = (256 - difference_w)/(width - 1)
-
-
-    left = 0
-    up = 0
-    right = 256
-    down = 256
-
-    img = Image.fromarray(np.array(img, dtype='uint8'))
-    img_mask = Image.fromarray(np.array(img_mask, dtype='uint8'))
-
-    for j in range(height):
-        for k in range(width):
-            dst = str(file_name)  + '_' + str(a) + '.png'
-            croppedImage = img.crop((left, up, right, down))
-            croppedImage_mask = img_mask.crop((left, up, right, down))
-            #croppedImage.show()
-            #img_array = np.array(img)
-            croppedImage_array = np.array(croppedImage_mask)
-            #print(" %d, %d,잘려진 사진 크기 :" %(k, a),croppedImage.size)
-
-            pixel_amount = np.count_nonzero(croppedImage_array == 255)
-
-            if (pixel_amount > ((256*256)*0.65)):
-                croppedImage.save(("/home/skim/NAS01/Users/skim/Tiger_crop/WSITILS/%s" % (dst)))
-            else:
-                print('%s는 백그라운드 비율이 더 크므로 저장 안됨'%(dst))
-            a = a + 1
-            left = left + (difference_w - overlap_w)
-            right = right + (difference_w - overlap_w)
-        up = up + (difference_h - overlap_h)
-        down = down + (difference_h - overlap_h)
-        left = 0
-        right = 256
-'''
